aa8f274ac3a4cd64da7c03b18be1b277e56b2cde
Lanreycole <47089721+Lanreycole@users.noreply.github.com>
2019-11-13 13:29:23 -0500

    Initial Commit

---
+++ electric_charger/src/main/java/com/example/electric_charger/Electric_charger.java
@@ -0,0 +1,14 @@
+package com.example.electric_charger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Electric_charger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_electric_charger);
+    }
+}
